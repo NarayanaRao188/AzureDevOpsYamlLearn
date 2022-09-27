c5add06649a21018e175f00624f63e83ab1a020a
NarayanaRao188 <98322134+NarayanaRao188@users.noreply.github.com>
2022-09-27 21:21:03 +0530

    Add files via upload

---
+++ stgacccreation.ps1
@@ -0,0 +1,9 @@
+﻿
+$resourceGroup = "demopsstg-rg"
+$location = "eastus"
+New-AzResourceGroup -Name $resourceGroup -Location $location
+New-AzStorageAccount -ResourceGroupName $resourceGroup `
+  -Name "demostgps123098" `
+  -Location $location `
+  -SkuName Standard_LRS `
+  -Kind StorageV2
